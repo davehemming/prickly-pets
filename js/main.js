ea1c3c6c973b8963fbebba029cd72d223afacec2
David Hemming <davehemming@gmail.com>
2014-10-23 11:33:19 +1100

    various layout concepts still under development

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
+	function resetHeights() {
+		$('.row').each(function() {
+			var height = $(this).innerHeight();
+
+			$(this).find('[class*="col-"]').each(function() {
+				$(this).css('height', height);
+			});
+
+			// console.log(height);
+		});
+	}
+
+	// resetHeights();
+
+	// $(window).resize(function() {
+	// 	resetHeights();
+	// });
 
 });
